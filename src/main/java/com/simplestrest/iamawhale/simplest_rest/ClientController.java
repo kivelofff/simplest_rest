b4d0278390ca7aa0e84c452ef548da7683690529
ladmin <kivelofff@gmail.com>
2020-10-26 20:34:24 +0100

    third

--- src/main/java/com/simplestrest/iamawhale/simplest_rest/ClientController.java
+++ src/main/java/com/simplestrest/iamawhale/simplest_rest/ClientController.java
@@ -5,8 +5,12 @@ import com.simplestrest.iamawhale.simplest_rest.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ClientController {
@@ -17,8 +21,18 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    @PostMapping(value = "/clients")
     public ResponseEntity<?> create(@RequestBody Client client) {
         clientService.create(client);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @GetMapping(value = "/clients\']" +
+            "uji                                                                                                         ")
+    public ResponseEntity<List<Client>> read() {
+        final List<Client> clients = clientService.readAll();
+
+        return clients != null && !clients.isEmpty()
+                ? new ResponseEntity<>(clients, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
